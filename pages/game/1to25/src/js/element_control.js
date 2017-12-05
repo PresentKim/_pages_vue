664b1608696327ba9f864eeeb4bb0027cef7af7a
PresentKim <qjrmckd@gmail.com>
2017-12-05 16:02:35 +0900

    Compile to ES5

--- pages/game/1to25/src/js/element_control.js
+++ pages/game/1to25/src/js/element_control.js
@@ -1,4 +1,4 @@
-// minifyOnSave, filenamePattern: ../../min/js/$1.$2, minifier: gcc, buffer: 8388608, minifierOptions: "charset = utf-8 nomunge"
+// minifyOnSave, filenamePattern: ../../min/js/$1.$2, minifier: gcc, buffer: 8388608, minifierOptions: "charset = utf-8 nomunge language_out=ES5"
 
 // Element Control
 var ElementControl = function(elementId, data, fomatter, filter) {
--- pages/game/1to25/src/js/game.js
+++ pages/game/1to25/src/js/game.js
@@ -1,4 +1,4 @@
-// minifyOnSave, filenamePattern: ../../min/js/$1.$2, minifier: gcc, buffer: 8388608, minifierOptions: "charset = utf-8 nomunge"
+// minifyOnSave, filenamePattern: ../../min/js/$1.$2, minifier: gcc, buffer: 8388608, minifierOptions: "charset = utf-8 nomunge language_out=ES5"
 
 var Game = function(xSize, ySize) {
   this.canStart = true;
