f8aa2fee8c335dfeecbf33acedec84afcfccd851
PresentKim <qjrmckd@gmail.com>
2017-12-12 00:05:35 +0900

    Easily change the ball generate

--- pages/test/canvas/rotate_ball/src/js/app.js
+++ pages/test/canvas/rotate_ball/src/js/app.js
@@ -35,19 +35,26 @@ function generate() {
   circles = [];
   var center = new Vector2(canvas.width / 2, canvas.height / 2);
   var relative = Math.sqrt(canvas.width * canvas.height, 2) / 3;
-  for (var degree = 0; degree < 360; degree += 15) {
-    var radian = (degree + 7.5) * Math.PI / 180;
-    var position = angleToDirection(radian).multiply(new Vector2(relative, relative), true).add(center, true);
-    balls.push(new Circle(position.x, position.y, 3));
+  for (var degree = 0; degree < 360; degree += 30) {
+    balls.push(new Circle(0, 0, 3));
+    balls.push(new Circle(0, 0, 3));
 
-    if (degree % 30 == 0) {
-      radian = degree * Math.PI / 180;
-      position = angleToDirection(radian).multiply(new Vector2(relative, relative), true).add(center, true);
-      var circle = new Circle(position.x, position.y, 13);
-      circle.angle = Math.PI / 12 * circles.length;
-      circles.push(circle);
-    }
+    var radian = degree * Math.PI / 180;
+    var position = angleToDirection(radian).multiply(new Vector2(relative, relative), true).add(center, true);
+    var circle = new Circle(position.x, position.y, 13);
+    circle.angle = Math.PI / 12 * circles.length;
+    circles.push(circle);
   }
+
+  for (i in circles) {
+    var targetBall = [balls[i * 2 + 1], balls[i * 2 - 2 < 0 ? 24 - i * 2 - 2 : i * 2 - 2]];
+    var relativeRadius = getRelativeSize(circles[i].radius);
+    var velocity = angleToDirection(circles[i].angle + i * Math.PI / 12).multiply(new Vector2(relativeRadius, relativeRadius), true);
+
+    targetBall[0].from(circles[i].add(velocity, true));
+    targetBall[1].from(circles[i].subtract(velocity, true));
+  }
+  
   render();
 }
 
