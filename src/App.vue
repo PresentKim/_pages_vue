bc2f20643633a9e2f246dfd00fb5eb0770d9bf36
PresentKim <qjrmckd@gmail.com>
2017-12-21 22:57:52 +0900

    Remove fixed, clipped

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
 <v-app dark>
-  <v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" app>
+  <v-navigation-drawer fixed :mini-variant="miniVariant" :clipped="true" v-model="drawer" app>
     <v-list>
       <v-list-tile value="true" v-for="(item, i) in items" :key="i">
         <v-list-tile-action>
@@ -15,16 +15,10 @@
     </v-list>
   </v-navigation-drawer>
 
-  <v-toolbar fixed app :clipped-left="clipped">
+  <v-toolbar fixed app :clipped-left="true">
     <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
     <v-btn icon @click.stop="miniVariant = !miniVariant">
       <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-    </v-btn>
-    <v-btn icon @click.stop="clipped = !clipped">
-      <v-icon>web</v-icon>
-    </v-btn>
-    <v-btn icon @click.stop="fixed = !fixed">
-      <v-icon>remove</v-icon>
     </v-btn>
     <v-spacer></v-spacer>
     <v-toolbar-title v-text="title"></v-toolbar-title>
@@ -48,7 +42,7 @@
     </v-container>
   </v-content>
 
-  <v-footer :fixed="fixed" app>
+  <v-footer app :fixed="true">
     <span>&copy; 2017</span>
   </v-footer>
 </v-app>
@@ -58,9 +52,7 @@
 export default {
   data() {
     return {
-      clipped: false,
       drawer: true,
-      fixed: false,
       items: [{
         icon: 'bubble_chart',
         title: 'Inspire'
