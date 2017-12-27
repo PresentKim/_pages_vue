f6ad5c67796bc6c60fe6ef9cab268e79198431a2
PresentKim <qjrmckd@gmail.com>
2017-12-27 17:47:42 +0900

    Update Main page

--- src/vue/router/pages/Main.vue
+++ src/vue/router/pages/Main.vue
@@ -1,8 +1,13 @@
 <template>
 <v-container fluid>
   <v-layout column align-center>
-    <img src="public/v.png" alt="main.png" class="mb-5" />
     <h1>Main Page</h1>
+    <img src="public/profile.png" alt="main.png" class="mb-5" />
+    <v-layout row align-center>
+      <v-icon x-large @click="$router.replace('/game')">gamepad</v-icon>
+      &nbsp;
+      <v-icon x-large @click="$router.replace('/canvas')">aspect_ratio</v-icon>
+    </v-layout>
   </v-layout>
 </v-container>
 </template>
@@ -20,3 +25,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+img {
+  border-radius: 50%;
+  height: 120px;
+  width: 120px;
+}
+</style>
