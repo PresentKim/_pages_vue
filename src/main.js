cfee8667dbb40c3b96070a40d2f9f0d7c6efeb89
PresentKim <qjrmckd@gmail.com>
2017-12-27 21:52:27 +0900

    Add vue-js-grid

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import VueHead from 'vue-head'
+import Grid from 'vue-js-grid'
 import router from './vue/router'
 import store from './vue/store'
 
@@ -13,6 +14,7 @@ Vue.use(VueHead, {
   separator: '-',
   complement: ''
 })
+Vue.use(Grid)
 
 new Vue({
   el: '#app',
