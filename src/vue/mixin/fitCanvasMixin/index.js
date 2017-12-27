c54bfec587cd3762c360d65d4fbc9d85d3afc334
PresentKim <qjrmckd@gmail.com>
2017-12-27 17:25:00 +0900

    Add fitCanvasMixin (from utils/canvasUtils)

---
+++ src/vue/mixin/fitCanvasMixin/index.js
@@ -0,0 +1,36 @@
+export default {
+  data() {
+    return {
+      relativeSize: null,
+    }
+  },
+  methods: {
+    updateRelativeSize: function(vector2Arr) {
+      if (vector2Arr) {
+        var lastRelativeSize = this.relativeSize;
+        this.updateRelativeSize(this);
+        if (this.relativeSize != lastRelativeSize) {
+          var changedRatio = this.relativeSize / lastRelativeSize;
+
+          for (var i in vector2Arr)
+            vector2Arr[i].multiply(changedRatio);
+        }
+      } else {
+        var canvas = this.$refs.canvas;
+        this.relativeSize = Math.sqrt(canvas.width * canvas.height, 2) / 100;
+      }
+      return this.relativeSize;
+    },
+
+    getRelativeSize: function(size) {
+      return size * this.relativeSize;
+    },
+
+    fitCanvasSize: function() {
+      var canvas = this.$refs.canvas;
+      var elements = this.$store.state.elements;
+      canvas.height = elements.footer.$el.getBoundingClientRect().top - elements.toolbar.$el.clientHeight;
+      canvas.width = elements.app.$el.clientWidth;
+    }
+  }
+}
